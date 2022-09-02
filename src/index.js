f5cd054f0a7710045cd135a96b6d0040fdaffb25
Ernest <ernest326648@gmail.com>
2022-09-03 00:06:09 +0100

    New init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { hop } from "@onehop/client";
+import App from './App';
+
+hop.init({
+	projectId: "project_NTAzMTk3MDc5ODI0MjI0NjE" // replace with your project ID
+});
+ 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
