253704b04605ed9953e58d4cfcb10c8f65a2a879
FrogDog56 <83395400+FrogDog56@users.noreply.github.com>
2022-09-03 01:03:19 +0100

    made some basic ui

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Test
-        </p>
-      </header>
+      <div className='Login-box'>
+        <div className='Login-box-title'>
+          <h1>SuperMaker</h1>
+        </div>
+        <div className='Login-box-middle'>
+          <div className='Login-box-middle-left'>
+            <button className='Login-btn'>Login</button>
+          </div>
+          <div className='Login-box-middle-right'>
+            <button className='Login-btn'>Register</button>
+          </div>
+        </div>
+        <div className='Login-box-lower'>
+          <button id='Guest'>Play as guest</button>
+        </div>
+      </div>
     </div>
   );
 }
