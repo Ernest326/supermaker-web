6744e7081f8ca9b306e9bc4be3748b8ab6812f1e
FrogDog56 <83395400+FrogDog56@users.noreply.github.com>
2022-09-03 12:15:46 +0100

    added react bootstrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { hop } from "@onehop/client";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+
 
 hop.init({
 	projectId: "project_NTAzMTk3MDc5ODI0MjI0NjE" // replace with your project ID
